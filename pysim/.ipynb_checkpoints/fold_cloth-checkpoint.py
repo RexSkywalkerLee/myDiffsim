dfee5c26d047050b94065283e336dee236e8b01b
20chase <tingxiangfan@gmail.com>
2020-08-11 11:48:38 +0800

    Lift_expert first exp: ends with TAUCS fail

--- pysim/.ipynb_checkpoints/fold_cloth-checkpoint.py
+++ pysim/.ipynb_checkpoints/fold_cloth-checkpoint.py
@@ -11,7 +11,9 @@ import numpy as np
 import os
 from datetime import datetime
 
-handles = [25, 30] 
+handles = [25, 60, 30, 54] 
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3'
 
 print(sys.argv)
 if len(sys.argv)==1:
@@ -26,21 +28,30 @@ timestamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
 torch_model_path = out_path + ('/net_weight.pth%s'%timestamp)
 
-
+#if torch.cuda.is_available():
+#    dev = "cuda:0"
+#else:
+#    dev = "cpu"
+#    torch.set_num_threads(8)
+#device = torch.device(dev)
+#
 
 class Net(nn.Module):
-	def __init__(self, n_input, n_output):
-		super(Net, self).__init__()
-		self.fc1 = nn.Linear(n_input, 50).double()
-		self.fc2 = nn.Linear(50, 200).double()
-		self.fc3 = nn.Linear(200, n_output).double()
-	def forward(self, x):
-		x = F.relu(self.fc1(x))
-		x = F.relu(self.fc2(x))
-		x = self.fc3(x)
-		# x = torch.clamp(x, min=-5, max=5)
-		return x
-
+    def __init__(self, n_input, n_output):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n_input, 500).double()
+        self.fc2 = nn.Linear(500, 200).double()
+        self.fc3 = nn.Linear(200, 50).double()
+        self.fc4 = nn.Linear(50, n_output).double()
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        x = x.cuda()
+        # x = torch.clamp(x, min=-5, max=5)
+        return x
+        
 with open('conf/rigidcloth/fold_cloth/fold_cloth.json','r') as f:
 	config = json.load(f)
 # matfile = config['cloths'][0]['materials'][0]['data']
@@ -49,13 +60,14 @@ with open('conf/rigidcloth/fold_cloth/fold_cloth.json','r') as f:
 
 
 goal = []
-with open('meshes/rigidcloth/fold_cloth/fold_cloth.obj','r') as f:
+with open('meshes/rigidcloth/drag/cloth.obj','r') as f:
     for line in f:
         if 'v ' in line:
             pos = [float(i) for i in line[2:].split()]
             new_pos = torch.tensor(pos, dtype=torch.float64)
             goal.append(new_pos)
 goal = torch.stack(goal)
+goal = goal.cuda()
 
 def save_config(config, file):
 	with open(file,'w') as f:
@@ -64,7 +76,6 @@ def save_config(config, file):
 save_config(config, out_path+'/conf.json')
 
 
-torch.set_num_threads(8)
 spf = config['frame_steps']
 
 scalev=1
@@ -91,6 +102,7 @@ def get_loss(ans, goal):
     diff = ans - goal
     diff[:,2] *= 10
     loss = torch.norm(diff)
+    loss = loss.cuda()
     
     #print(ans)
     #print(goal)
@@ -112,13 +124,14 @@ def run_sim(steps, sim, net, goal):
         remain_time = torch.tensor([(steps - step)/steps],dtype=torch.float64)
         		
         net_input = []
-        for i in range(len(handles)):
-            net_input.append(sim.cloths[0].mesh.nodes[handles[i]].x)
-            net_input.append(sim.cloths[0].mesh.nodes[handles[i]].v)
+        for node in sim.cloths[0].mesh.nodes:
+            net_input.append(node.x)
+            net_input.append(node.v)
 
         # dis = sim.obstacles[0].curr_state_mesh.dummy_node.x - goal
         # net_input.append(dis.narrow(0, 3, 3))
         net_input.append(remain_time)
+		net_input = [t.cuda() for t in net_input]
         net_output = net(torch.cat(net_input))
         		
         
@@ -126,6 +139,7 @@ def run_sim(steps, sim, net, goal):
         		
         for i in range(len(handles)):
             sim_input = net_output[3*i:3*i+3]
+            sim_input = sim_input.to(torch.device('cpu'))
             sim.cloths[0].mesh.nodes[handles[i]].v += sim_input 
         
         arcsim.sim_step()
@@ -135,6 +149,7 @@ def run_sim(steps, sim, net, goal):
 
     ans = [ node.x for node in sim.cloths[0].mesh.nodes ]
     ans = torch.stack(ans)
+    ans = ans.cuda()
 
     #ans1 /= cnt
     #ans1 = torch.cat([torch.tensor([0, 0, 0],dtype=torch.float64), ans1])
@@ -151,7 +166,7 @@ def do_train(cur_step,optimizer,sim,net):
     global goal
     while True:
         #steps = int(1*15*spf)
-        steps = 50
+        steps = 20
         
         #sigma = 0.05
         #z = np.random.random()*sigma + 0.5
@@ -187,7 +202,7 @@ def do_train(cur_step,optimizer,sim,net):
         if epoch % 5 == 0:
             torch.save(net.state_dict(), torch_model_path)
         
-        if loss<1e-2:
+        if loss<1e-3:
             break
         # dgrad, stgrad, begrad = torch.autograd.grad(loss, [density, stretch, bend])
         
@@ -200,24 +215,26 @@ def do_train(cur_step,optimizer,sim,net):
         # break
 
 with open(out_path+('/log%s.txt'%timestamp),'w',buffering=1) as f:
-	tot_step = 1
-	sim=arcsim.get_sim()
-	# reset_sim(sim)
-
-	#param_g = torch.tensor([0,0,0,0,0,1],dtype=torch.float64, requires_grad=True)
-	net = Net(13, 6)
-	if os.path.exists(torch_model_path):
-		net.load_state_dict(torch.load(torch_model_path))
-		print("load: %s\n success" % torch_model_path)
-
-	lr = 0.01
-	momentum = 0.9
-	f.write('lr={} momentum={}\n'.format(lr,momentum))
-	#optimizer = torch.optim.SGD([{'params':net.parameters(),'lr':lr}],momentum=momentum)
-	optimizer = torch.optim.Adam(net.parameters(),lr=lr)
-	# optimizer = torch.optim.Adadelta([density, stretch, bend])
-	for cur_step in range(tot_step):
-		do_train(cur_step,optimizer,sim,net)
-
+    tot_step = 1
+    sim=arcsim.get_sim()
+    # reset_sim(sim)
+    
+    #param_g = torch.tensor([0,0,0,0,0,1],dtype=torch.float64, requires_grad=True)
+    net = Net(487, 12)
+    net = nn.DataParallel(net)
+    net = net.cuda()
+    if os.path.exists(torch_model_path):
+        net.load_state_dict(torch.load(torch_model_path))
+        print("load: %s\n success" % torch_model_path)
+    
+    lr = 0.01
+    momentum = 0.9
+    f.write('lr={} momentum={}\n'.format(lr,momentum))
+    #optimizer = torch.optim.SGD([{'params':net.parameters(),'lr':lr}],momentum=momentum)
+    optimizer = torch.optim.Adam(net.parameters(),lr=lr)
+    # optimizer = torch.optim.Adadelta([density, stretch, bend])
+    for cur_step in range(tot_step):
+        do_train(cur_step,optimizer,sim,net)
+    
 print("done")
 
